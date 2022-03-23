5f02c132397244a79d18766f1e7094a64bcaf00d
Tran Ngoc Thien <thientn@midota.com>
2022-03-23 19:33:18 +0700

    updates layout

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Home.scss";
+import { homeLink } from "./fake-data";
+import LinkItem from "./components/LinkItem/LinkItem";
+import {avatar} from 'assets/images'
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="container">
+        <div className="home-specialBg" />
+        <div className="home-wrap">
+          <div className="quick-profile">
+              <div className="quick-profile-wrap">
+                <div className="quick-profile__left">
+                  <h1>Fuze Thien</h1>
+                  <h4>Software Engineer & UI/UX Expert</h4>
+                  <div className="left-link">
+                    {homeLink &&
+                      homeLink.map((item, i) => (
+                        <LinkItem key={i} value={item} />
+                      ))}
+                  </div>
+                  <div className="quick-profile__left-splash"></div>
+                </div>        
+                <div className="quick-profile__right">
+                  <img src={avatar} alt="avatar" />
+                </div>
+
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Home/fake-data.js
@@ -0,0 +1,25 @@
+import { IoMailSharp } from "react-icons/io5";
+import { AiOutlineGlobal, AiFillSkype,AiFillPhone } from "react-icons/ai";
+import {MdPlace} from 'react-icons/md'
+export const homeLink = [
+  {
+    icon: <IoMailSharp />,
+    data: "job.tranthien@gmail.com",
+  },
+  {
+    icon: <AiOutlineGlobal />,
+    data: "job.tranthien@gmail.com",
+  },
+  {
+    icon: <AiFillSkype />,
+    data: "tranthien@skype.com",
+  },
+  {
+    icon: <AiFillPhone />,
+    data: "+84 913184232",
+  },
+  {
+    icon: <MdPlace />,
+    data: "286a Ta Quang Buu Street, Ward 4, District 8, Ho Chi Minh city",
+  },
+];
