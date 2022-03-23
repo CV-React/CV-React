ed58d410afca68d28aeef556a5e985f5841c39c3
Tran Ngoc Thien <thientn@midota.com>
2022-03-23 20:54:50 +0700

    git

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./Home.scss";
-import { homeLink } from "./fake-data";
+import { homeLink, socialIcon } from "./fake-data";
 import LinkItem from "./components/LinkItem/LinkItem";
-import {avatar} from 'assets/images'
+import { GoPlusSmall } from "react-icons/go";
+
 const Home = () => {
   return (
     <div className="home">
@@ -10,24 +11,54 @@ const Home = () => {
         <div className="home-specialBg" />
         <div className="home-wrap">
           <div className="quick-profile">
-              <div className="quick-profile-wrap">
-                <div className="quick-profile__left">
-                  <h1>Fuze Thien</h1>
-                  <h4>Software Engineer & UI/UX Expert</h4>
-                  <div className="left-link">
-                    {homeLink &&
-                      homeLink.map((item, i) => (
-                        <LinkItem key={i} value={item} />
-                      ))}
-                  </div>
-                  <div className="quick-profile__left-splash"></div>
-                </div>        
-                <div className="quick-profile__right">
-                  <img src={avatar} alt="avatar" />
-                  ad
+            <div className="quick-profile-wrap">
+              <div className="quick-profile__left">
+                <span className="plus">
+                  <GoPlusSmall />
+                </span>
+                <h1>Fuze Thien</h1>
+                <h4>Software Engineer & UI/UX Expert</h4>
+                <div className="left-link">
+                  {homeLink &&
+                    homeLink.map((item, i) => (
+                      <LinkItem key={i} value={item} />
+                    ))}
                 </div>
-
+                <div className="social">
+                  {socialIcon &&
+                    socialIcon.map((e, i) => (
+                      <span className="social-icon" key={i}>
+                        {e.icon}
+                      </span>
+                    ))}
+                </div>
+                <div className="quick-profile__left-splash"></div>
               </div>
+              <div className="quick-profile__right"></div>
+            </div>
+          </div>
+          <div className="mt-50 about">
+            <div className="about-wrap">
+              <div className="about-text">
+                <p>
+                  Hello! I’m John Doe. Senior Web Developer with over 13 years
+                  of experience specializing in front end development.
+                  Experienced with all stages of the development cycle for
+                  dynamic web projects.Having an in-depth knowledge including
+                  advanced HTML5, CSS, CSS3, SASS, LESS, JSON, XML, Java,
+                  JavaScript, JQuery, Angular JS. Strong background in
+                  management and leadership.
+                </p>
+              </div>
+              <div className="about-cv">
+               <button type="button" className="btn btn-primary mr-20">DOWNLOAD CV</button>
+               <button type="button" className="btn btn-primary">CONTACT ME</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-50 education">
+           Education Block
           </div>
         </div>
       </div>
